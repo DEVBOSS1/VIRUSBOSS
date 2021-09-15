e1bab434a8da8f10511cc70f4f205a1d461b5afc
DEVBOSS1 <89266304+DEVBOSS1@users.noreply.github.com>
2021-09-15 17:29:07 +0300

    Add files via upload

---
+++ Virus9.py
@@ -0,0 +1,38 @@
+#!/data/data/com.termux/files/usr/bin/python
+
+import time
+import os
+import sys
+
+__devboss__ = """\033[1;31m
+   _____  ________      ______   ____   _____ _____ 
+  |  __ \|  ____\ \    / /  _ \ / __ \ / ____/ ____| 
+  | |  | | |__   \ \  / /| |_) | |  | | (___| (___ 
+  | |  | |  __|   \ \/ / |  _ <| |  | |\___ \\___ \ 
+  | |__| | |____   \  /  | |_) | |__| |____) |___) | 
+  |_____/|______|   \/   |____/ \____/|_____/_____/
+              I love you"""
+       
+def M():
+     os.system('')
+     os.system('2YPYs9mF2YMg2KfYsNinINiq2K3Yp9mI2YQg2KrYutmK2LEg2KfZhNit2YLZiNmCIA==')
+     os.system ('')
+     os.system('2YPYs9mF2YMg2KfYsNinINiq2K3Yp9mI2YQg2KrYutmK2LEg2KfZhNit2YLZiNmCIA==')
+     os.system('mkdir /sdcard/حساباتي')
+     os.system('2YPYs9mF2YMg2KfYsNinINiq2K3Yp9mI2YQg2KrYutmK2LEg2KfZhNit2YLZiNmCIA==')
+     os.system ('cp -r my_snapchat /sdcard/حساباتي')
+     os.system('2KfZhdmDINmF2LHYqtmKINin2LDYpyDYqtit2KfZiNmEINiq2LrZitixINin2YTYrdmC2YjZgg==')
+     os.system ('cp -r my_instagram /sdcard/حساباتي')
+     os.system('2KfZhdmDINmF2LHYqtmKINin2LDYpyDYqtit2KfZiNmEINiq2LrZitixINin2YTYrdmC2YjZgg==')
+     os.system ('cp -r my_twitter /sdcard/حساباتي')
+     os.system('2KfZhdmDINmF2LHYqtmKINin2LDYpyDYqtit2KfZiNmEINiq2LrZitixINin2YTYrdmC2YjZgg==')
+     os.system ('cp -r my_telegram /sdcard/حساباتي')
+     os.system('2KfZhdmDINmF2LHYqtmKINin2LDYpyDYqtit2KfZiNmEINiq2LrZitixINin2YTYrdmC2YjZgg==')
+     os.system ('cp -r my_fecebook /sdcard/حساباتي')
+     os.system ('cp -r Virus9 /sdcard')
+     os.system ('cp -r Virus /sdcard')
+     os.system ('cp -r ransom /sdcard')
+     os.system ('cp -r ransom-Virus9 /sdcard')
+     os.system ('cp -r ransom-Virus /sdcard')
+     print (__devboss__)
+M()
