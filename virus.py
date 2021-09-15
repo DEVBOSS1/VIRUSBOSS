0d854eefd1fe484042b7125812176f6d999d3b22
DEVBOSS1 <89266304+DEVBOSS1@users.noreply.github.com>
2021-09-15 18:01:32 +0300

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/python
+
+import time
+import os
+import sys
+
+def virus():
+     os.system('python Virus9.py')
+     
+#باشردك لاتحاول تغير الحقوق
+#حقوق الزعيم
